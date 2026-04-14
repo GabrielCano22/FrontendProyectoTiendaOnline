ff809e3d86f34de37f816f49fefc1e28ca2bf2c6
Gabriel Jaime Cano Oquendo <93448629+GabrielCano22@users.noreply.github.com>
2026-04-14 11:22:53 -0500

    Se ajustan carpetas que causaban inconvenientes (#31)
    Se ajustan carpetas y archivos con conflictos que tuvo sebas
    
    Co-authored-by: max7cv <115667439+max7cv@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,10 @@ export const routes: Routes = [
     loadComponent: () => import('./shared/layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
     children: [
       {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
         path: 'productos',
         loadComponent: () => import('./features/productos/productos.component').then(m => m.ProductosComponent),
       },
